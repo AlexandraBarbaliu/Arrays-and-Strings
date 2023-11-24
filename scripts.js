4f74951a1b56f40c7b10331d10edb676147edd63
AlexandraBarbaliu <144890724+AlexandraBarbaliu@users.noreply.github.com>
2023-11-24 20:15:47 +0200

    inputAsArray

--- scripts.js
+++ scripts.js
@@ -2,9 +2,22 @@ const personObject = {
     name: 'John',
     age: 30,
     hobby: ['hiking', 'dogs', 'music'],
-    introduction: function() {
+    introduction: function () {
         return `Salut! Numele meu este: ${this.name}. Am ${this.age} ani, iar ca hobby pot sa spun ca ma pasioneaza: ${this.hobby.join(', ')}.`;
     }
 };
 
 console.log(personObject.introduction())
+
+//---------------------------------------------------------------------------
+
+// let input = prompt('Introdu sirul de caractere');
+let input = 'Test string';
+
+// console.log(input splitted:', input.split(' '))
+let inputAsArray = input.split(' ');
+for(let i = 0; i < inputAsArray.length; i++) {
+
+    console.log(`Cuvantul: ${inputAsArray[i]} are lungimea de ${inputAsArray[i].lenght} caractere.`)
+
+}
